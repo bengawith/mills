c7f8d509c8301d480624c5cca5647db0f6c864e9
bengawith <biggawith@gmail.com>
2025-08-18 13:45:26 +0100

    updated mills csv's

--- backend/scripts/merge_csv.py
+++ backend/scripts/merge_csv.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import os
 
+
 panes: list[pd.DataFrame] = []
 for file in os.listdir('data'):
-    df: pd.DataFrame = pd.read_csv(os.path.join('data', file))
-    panes.append(df)
+    if file.startswith('mill_') and file.endswith('.csv'):
+        df: pd.DataFrame = pd.read_csv(os.path.join('data', file), encoding='utf-8')
+        panes.append(df)
 
 df: pd.DataFrame = pd.concat(panes)
 df.drop_duplicates(inplace=True)
